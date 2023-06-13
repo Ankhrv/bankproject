4922f5db21ff94e1f9bf0974d3ff1b13e069451e
AnKhrv <serik.avakyan@mail.ru>
2023-06-13 20:30:23 +0300

    5commit

--- src/Form/Wind3.jsx
+++ src/Form/Wind3.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import сp from './Wind3.module.css';
-
+import './w3.css';
 
 
 const Wind3 = () => {
     return (
          <div className={сp.oo}> 
 
-         <div className={сp.e1}>uyguigyg
+         <div className={сp.e1}>
+            <div ><p className='kll'>Обратная связь</p>
+            <p className='kll2'>+7 (937) 263-90-10 </p>
+            <p className='kll2'>harmonY64@yandex.ru</p>
+            <p className='kll2'>Предоставляем услуги в РФ, г. Саратов и области</p>
+            <p className='kll2'>Понедельник - Пятница: 9:00 - 18:00. </p>
+            <input className='knop' type="submit" value="Заказать звонок"></input></div>
+            <div ></div>
 
             </div></div>
 
--- src/Form/Wind4.jsx
+++ src/Form/Wind4.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import ao from './Wind4.module.css';
 
+import './Wind4.css';
 const Wind4 = () => {
     return (
-        <div className={ao.te}>
-        <div className={ao.text}>ygygilhuoh
 
-                </div>
-                </div>
+<form className='te' action="/" method="post">
+<p className='ui'>Хотите получить письменный ответ на вопрос? Напишите нам на электронную почту.</p>
+<input type="text" placeholder="Ваше имя"></input>
+<input type="email" placeholder="Электронная почта"></input>
+<textarea placeholder="Ваш вопрос или сообщение"></textarea>
+
+    <input className='knop' type="submit" value="Отправить"></input>
+  <p className='Tt'>
+
+Хотите получить письменный ответ на вопрос? Напишите нам на электронную почту.
+Нажимая кнопку "Отправить" вы подтверждаете что ознакомились и согласны с политикой конфиденциальности </p>
+  </form>
+
+ 
+  
     );
 }
 export default Wind4;
