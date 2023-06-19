680347aea35324cc1f04e00fd19e2fa2c9320f52
AnKhrv <serik.avakyan@mail.ru>
2023-06-20 00:34:33 +0300

    10000commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import For from './Str4/For';
 import Five from './Str5/Str5';
 import Form from './Form/Form';
 import Six from './Six/Six';
+import Seven from './Str7/Str7';
 
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
     <Five/>
     <Form/>
     <Six/>
+    <Seven/>
     
     </div>
   );
--- src/Form/Wind3.jsx
+++ src/Form/Wind3.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import сp from './Wind3.module.css';
 import './w3.css';
+import i1 from '../Str7/icons82.png' ;
+import i2 from '../Str7/icons8.png' ;
+import i3 from '../Str7/icons81.png' ;
+import i4 from '../Str7/icons83png.png' ;
 
 
 const Wind3 = () => {
@@ -9,10 +13,10 @@ const Wind3 = () => {
 
          <div className={сp.e1}>
             <div ><p className='kll'>Обратная связь</p>
-            <p className='kll2'>+7 (937) 263-90-10 </p>
-            <p className='kll2'>harmonY64@yandex.ru</p>
-            <p className='kll2'>Предоставляем услуги в РФ, г. Саратов и области</p>
-            <p className='kll2'>Понедельник - Пятница: 9:00 - 18:00. </p>
+            <p className='kll2'> <img src={i2} alt='i2' className="ii1"></img>+7 (937) 263-90-10 </p>
+            <p className='kll2'><img src={i3} alt='i2' className="ii1"></img>harmonY64@yandex.ru</p>
+            <p className='kll2'><img src={i1} alt='i1' className="ii1"></img>Предоставляем услуги в РФ, г. Саратов и области</p>
+            <p className='kll2'><img src={i4} alt='i2' className="ii1"></img>Понедельник - Пятница: 9:00 - 18:00. </p>
             <button className='knop' type="submit" value="Заказать звонок">Заказать звонок</button></div>
             <div ></div>
 
