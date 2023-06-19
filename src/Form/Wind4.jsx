2fa9f18762d34626708284f1b661920a0a643049
AnKhrv <serik.avakyan@mail.ru>
2023-06-19 18:04:16 +0300

    1906commit

--- src/Form/Wind4.jsx
+++ src/Form/Wind4.jsx
@@ -1,35 +1,44 @@
 import { useRef } from 'react';
+import React, { useState } from "react"
 import emailjs from '@emailjs/browser';
 import './Wind4.css';
-const Wind4 = () => {
-
-
+// import { Button } from '@react-email/button';
+const Wind4 = (props) => {
   const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
 
-    emailjs.sendForm('service_hmoh4pe', 'template_si5n3b9', form.current, 'pBgFTDGTqgd6JkT9M')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-    e.target.reset()
-  };
+  const [buttonText, setButtonText] = useState("Отправить");
+
+  const sendEmail  = (e)  => {
+   
+    e.preventDefault();
+    // setButtonText("загрузка...");
+    setButtonText("отправлено");
+    emailjs.sendForm('service_hmoh4pe','template_si5n3b9', form.current, 'pBgFTDGTqgd6JkT9M')
+    .then((result) => {
+      setButtonText("отправить");
+      console.log(result.text);
+      e.target.reset();
+  }, (error) => {
+      console.log(error.text); 
+      setButtonText("Sending Failed");
+  });
+ 
+};
+
 
   return (
-   <div className='GL'> <form ref={form} onSubmit={sendEmail} className='te'>
+    <div className='GL'> <form ref={form} onSubmit={sendEmail} className='te'>
       <p className='ui'>Хотите получить письменный ответ на вопрос? Напишите нам на электронную почту.</p>
       <input type="text" placeholder="Ваше имя" name="us_name" required />
       <input type="email" placeholder="Электронная почта" name="us_email" required />
       <textarea placeholder="Ваш вопрос или сообщение" name="message"></textarea>
-      <button className='knop4' type="submit" value="Отправить"> Отправить</button>
+      <button  className='knop4' type="submit" value="Отправить">{buttonText} 
+      </button>
       <p className='Tt'>
         Хотите получить письменный ответ на вопрос? Напишите нам на электронную почту.
         Нажимая кнопку "Отправить" вы подтверждаете что ознакомились и согласны с политикой конфиденциальности </p>
     </form>
     </div>
-
 
   );
 }
